084ce6c2ca7eff001cb754d6ea29d125d87c2ba4
Karolina <black-karolina@mail.ru>
2026-04-10 03:28:37 +0300

    refactoring program

--- GuessTheWord/Program.cs
+++ GuessTheWord/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace GuessTheWord
 {
@@ -8,6 +9,32 @@ namespace GuessTheWord
         {
             var game = new Game(attemptsMax:10);
             game.AddLetter('a', isGuessed: true);
+            
+            
+            var usedLetters = new HashSet<char>();
+            usedLetters.Add('a');
+            usedLetters.Add('b');
+            
+            char newLetter = 'a';
+            
+            if (!usedLetters.Contains(newLetter))
+                usedLetters.Add('a');
+            
+            var dictionary =  new Dictionary<DifficultyType, int>();
+            dictionary[DifficultyType.Easy] = 4;
+            dictionary[DifficultyType.Normal] = 2;
+            dictionary[DifficultyType.Hard] = 0;
+
+            if (dictionary.TryGetValue(DifficultyType.Easy, out int result))
+            {
+                
+            }
+
+        }
+
+        private void InitCharactersArray(int maxAttempts)
+        {
+            var characters = new char[maxAttempts];
         }
     }
 }
--- GuessTheWord/WordBank.cs
+++ GuessTheWord/WordBank.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace GuessTheWord
@@ -6,10 +7,27 @@ namespace GuessTheWord
     public class WordBank
     {
         private Word[] _words;
+        private List<Word> _wordsList;
 
         public WordBank()
         {
             _words = new[]
+            {
+                new Word("ide"),
+                new Word("cat"),
+                new Word("dog"),
+                new Word("home"),
+                new Word("cold"),
+                new Word("unity"),
+                new Word("laptop"),
+                new Word("family"),
+                new Word("teacher"),
+                new Word("computer"),
+                
+                
+            };
+            
+            _wordsList = new List<Word>()
             {
                 new Word("ide"),
                 new Word("cat"),
